90549ec3e9d8c778be81831dceb134203144f142
Yonel Ceruto <yonel.ceruto@scnd.com>
2025-06-16 18:40:19 -0400

    add web profiler

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Yceruto\FormFlowBundle\FormFlowBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
